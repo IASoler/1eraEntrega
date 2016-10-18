4231b5546570f069db9ed7d7ab50a04d1dfb4cbf
ARNAU SISTACH REINOSO <asistare7.alumnes@ub052229.mat.ub.edu>
2016-10-18 17:23:00 +0200

    Segona entrega, inici

--- projectParams.py
+++ projectParams.py
@@ -13,7 +13,7 @@
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 
-STUDENT_CODE_DEFAULT = 'searchAgents.py,search.py'
-PROJECT_TEST_CLASSES = 'searchTestClasses.py'
-PROJECT_NAME = 'Project 1: Search'
+STUDENT_CODE_DEFAULT = 'multiAgents.py'
+PROJECT_TEST_CLASSES = 'multiagentTestClasses.py'
+PROJECT_NAME = 'Project 2: Multiagent search'
 BONUS_PIC = False
